e60a7c5674e45ebc0e211f61973ea024192a6c6b
Dazbo <derailed.dash@gmail.com>
2026-01-17 23:11:43 +0000

    conductor(checkpoint): Checkpoint end of Phase 2

--- deployment/terraform/storage.tf
+++ deployment/terraform/storage.tf
@@ -1,17 +1,3 @@
-# Copyright 2025 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 provider "google" {
   region = var.region
   user_project_override = true
@@ -38,21 +24,11 @@ resource "google_artifact_registry_repository" "repo-artifacts-genai" {
 }
 
 resource "google_firestore_database" "database" {
-
   for_each                    = local.deploy_project_ids
-
   project                     = each.value
-
   name                        = "(default)"
-
   location_id                 = var.region
-
   type                        = "FIRESTORE_NATIVE"
-
   delete_protection_state     = "DELETE_PROTECTION_DISABLED"
-
-  
-
   depends_on = [resource.google_project_service.deploy_project_services]
-
 }
--- deployment/terraform/vars/env.tfvars
+++ deployment/terraform/vars/env.tfvars
@@ -2,18 +2,19 @@
 project_name = "dazbo-portfolio"
 
 # Your Production Google Cloud project id
-prod_project_id = "your-production-project-id"
+prod_project_id = "dazbo-portfolio"
 
 # Your Staging / Test Google Cloud project id
-staging_project_id = "your-staging-project-id"
+staging_project_id = "dazbo-portfolio"
 
 # Your Google Cloud project ID that will be used to host the Cloud Build pipelines.
-cicd_runner_project_id = "your-cicd-project-id"
-# Name of the host connection you created in Cloud Build
-host_connection_name = "git-dazbo-portfolio"
-github_pat_secret_id = "your-github_pat_secret_id"
+cicd_runner_project_id = "dazbo-portfolio"
 
-repository_owner = "Your GitHub organization or username."
+# Name of the host connection you created in Cloud Build
+host_connection_name = "dazbo-portfolio-gh-conn"
+github_pat_secret_id = "dazbo-portfolio-gh-conn-github-oauthtoken-5a4dea"
+
+repository_owner = "derailed-dash"
 
 # Name of the repository you added to Cloud Build
 repository_name = "dazbo-portfolio"
